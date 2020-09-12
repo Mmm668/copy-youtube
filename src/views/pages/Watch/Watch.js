6c2c2271da36cb7c8c9d63018e193622a425bad4
BY <devblossom77@gmail.com>
2020-09-12 23:30:45 +0900

    Youtube Iframe RWD 구현

--- src/views/pages/Watch/Watch.js
+++ src/views/pages/Watch/Watch.js
@@ -2,52 +2,62 @@ import React, {useEffect, useRef, useState} from 'react';
 import styled from "styled-components";
 import VideoPlaylist from "../../../components/Video/VideoPlaylist";
 
+let resWidth;
+let resHeight;
 const Watch = (props) => {
 
-<<<<<<< HEAD
-    return (
-        <Wrapper>
-            <VideoPanel>
-                <Video src={'https://www.w3schools.com/html/mov_bbb.mp4'}/>
-=======
     const videoArea = useRef(null);
     const [videoSize, setVideoSize] = useState({});
 
+    function resizeVideo() {
+        if(window.innerWidth <= 1175 && window.innerWidth >= 744){
+            resWidth = Math.floor(750 * window.innerWidth / 1175)
+            resHeight = Math.floor(resWidth * 360 / 640)
+        }
+        if(window.innerWidth < 744){
+            resWidth = 451
+        }
+        if(window.innerWidth > 1175){
+            resWidth = 744
+        }
+
+        resHeight = Math.floor(resWidth * 360 / 640)
+        setVideoSize({width: resWidth, height: resHeight})
+        setVideoSize({width: resWidth, height: resHeight})
+    }
+
     useEffect(() => {
+        window.addEventListener('resize', resizeVideo)
         if (videoArea) {
-            let resWidth = videoArea.current.offsetWidth - 48;
-            let resHeight = Math.floor(resWidth * 360 / 640)
-            setVideoSize({width: resWidth, height: resHeight})
+            resizeVideo()
+        }
+
+        return () => {
+            window.removeEventListener('resize', resizeVideo)
         }
     }, [videoArea])
 
     return (
         <Wrapper>
             <VideoPanel ref={videoArea}>
-                <Video src={'https://www.w3schools.com/html/mov_bbb.mp4'} style={videoSize}/>
->>>>>>> fbed946... [page] Watch width 계산
+                <iframe width={videoSize.width} height={videoSize.height} src="//www.youtube.com/embed/ue-0o-Glsrk"
+                        frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen/>
             </VideoPanel>
-            <VideoPlaylist></VideoPlaylist>
+            <VideoPlaylist className={'rwd'}></VideoPlaylist>
         </Wrapper>
     )
 };
 
 const Wrapper = styled.div`
   display: flex;
+  .rwd{
+    display: none;
+  }
 `;
 const VideoPanel = styled.div`
   flex-grow: 1;
-<<<<<<< HEAD
   padding: 21px 24px;
-`;
-const Video = styled.video`
-  width: 100%;
-  height: 100%;
-=======
-  padding: 24px;
-`;
-const Video = styled.video`
->>>>>>> fbed946... [page] Watch width 계산
 `;
 
 export default Watch;
