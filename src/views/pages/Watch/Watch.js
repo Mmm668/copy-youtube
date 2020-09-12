adb95f804ff53572de29f2c70e40a63b1df0358d
BY <devblossom77@gmail.com>
2020-09-12 22:15:08 +0900

    [page] Watch width 계산
    # Conflicts:
    #	src/views/pages/Watch/Watch.js

--- src/views/pages/Watch/Watch.js
+++ src/views/pages/Watch/Watch.js
@@ -4,10 +4,28 @@ import VideoPlaylist from "../../../components/Video/VideoPlaylist";
 
 const Watch = (props) => {
 
+<<<<<<< HEAD
     return (
         <Wrapper>
             <VideoPanel>
                 <Video src={'https://www.w3schools.com/html/mov_bbb.mp4'}/>
+=======
+    const videoArea = useRef(null);
+    const [videoSize, setVideoSize] = useState({});
+
+    useEffect(() => {
+        if (videoArea) {
+            let resWidth = videoArea.current.offsetWidth - 48;
+            let resHeight = Math.floor(resWidth * 360 / 640)
+            setVideoSize({width: resWidth, height: resHeight})
+        }
+    }, [videoArea])
+
+    return (
+        <Wrapper>
+            <VideoPanel ref={videoArea}>
+                <Video src={'https://www.w3schools.com/html/mov_bbb.mp4'} style={videoSize}/>
+>>>>>>> fbed946... [page] Watch width 계산
             </VideoPanel>
             <VideoPlaylist></VideoPlaylist>
         </Wrapper>
@@ -19,11 +37,17 @@ const Wrapper = styled.div`
 `;
 const VideoPanel = styled.div`
   flex-grow: 1;
+<<<<<<< HEAD
   padding: 21px 24px;
 `;
 const Video = styled.video`
   width: 100%;
   height: 100%;
+=======
+  padding: 24px;
+`;
+const Video = styled.video`
+>>>>>>> fbed946... [page] Watch width 계산
 `;
 
 export default Watch;
